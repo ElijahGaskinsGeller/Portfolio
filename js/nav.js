73d62dc332e898232c88cc328efc7a92af794e44
Elijah Gaskins-Geller <elijahgaskinsgeller@gmail.com>
2022-03-02 09:30:22 -0800

    Updated Site

--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,33 @@
 $(document).ready(function () {
 
-    $('.nav-toggle').on('click', function () {
-        $('.nav-toggler-anim').toggleClass('open');
-        $('.main-nav-container').toggleClass('open');
+    $('.header-nav-bars').on('click', function () {
+
+        $('.header-nav-popup').toggleClass('header-nav-hidden');
+        $('.header-nav-toggler-icon').toggleClass('d-none');
+        $('.header-nav-close-icon').toggleClass('d-none');
     });
 
+    $(window).on("scroll", function(e){
+
+        let nav = $("nav");
+
+        if(!nav.hasClass("scrolled") && e.currentTarget.pageYOffset > 150){
+
+            nav.addClass("scrolled");
+
+        }else if(nav.hasClass("scrolled") && e.currentTarget.pageYOffset <= 100){
+
+            nav.removeClass("scrolled");
+
+        }
+        // console.log(e);
+
+    });
+
+    console.log(window.scrollY);
+    if(window.scrollY > 150){
+
+        $("nav").addClass("scrolled");
+
+    }
 });
